f2d87337b47b5d93d7ef49f3ffed9819bf3eb710
William Hamparsomian <william.hamparsomian@svt.se>
2020-09-20 17:09:39 +0200

    Fix failing build

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,23 +4,24 @@ import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
-    {gridItems.map((item) => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
+    {gridItems &&
+      gridItems.map((item) => (
+        <div key={item.text} className="column is-6">
+          <section className="section">
+            <div className="has-text-centered">
+              <div
+                style={{
+                  width: '240px',
+                  display: 'inline-block',
+                }}
+              >
+                <PreviewCompatibleImage imageInfo={item} />
+              </div>
             </div>
-          </div>
-          <p>{item.text}</p>
-        </section>
-      </div>
-    ))}
+            <p>{item.text}</p>
+          </section>
+        </div>
+      ))}
   </div>
 )
 
