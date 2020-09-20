593cc4c4b7868269be8a38e91662c5a59fb5d9ee
William Hamparsomian <william.hamparsomian@svt.se>
2020-09-20 16:38:43 +0200

    Remove featured image

--- src/components/JoyRoll.js
+++ src/components/JoyRoll.js
@@ -91,13 +91,13 @@ export default () => (
                 templateKey
                 date(formatString: "MMMM DD, YYYY")
                 featuredpost
-                featuredimage {
-                  childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
-                      ...GatsbyImageSharpFluid
-                    }
-                  }
-                }
+                # featuredimage {
+                #   childImageSharp {
+                #     fluid(maxWidth: 120, quality: 100) {
+                #       ...GatsbyImageSharpFluid
+                #     }
+                #   }
+                # }
               }
             }
           }
